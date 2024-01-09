35119431ac2e3a7bae5eb5721cbb7fb438d10bfe
slogup-dale <dyddn304@gmail.com>
2024-01-09 14:45:23 +0900

    build: craco 설치

---
+++ craco.config.js
@@ -0,0 +1,22 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const CracoEnvPlugin = require('craco-plugin-env');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: {
+        overrideWebpackConfig: ({ webpackConfig }) => {
+          webpackConfig.resolve.plugins.push(new TsconfigPathsPlugin({}));
+          return webpackConfig;
+        },
+      },
+    },
+    {
+      plugin: CracoEnvPlugin,
+      options: {
+        variables: {},
+      },
+    },
+  ],
+};
