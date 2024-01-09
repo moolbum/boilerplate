1e15ef28c1a3cbc6db5c6eb134ce38a634953986
slogup-dale <dyddn304@gmail.com>
2024-01-09 14:16:34 +0900

    build: prettier 수정

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  tabWidth: 2, // 탭너비 2로 설정
+  endOfLine: 'lf', // EoF 방식, OS별로 처리 방식이 다름
+  arrowParens: 'avoid', // 화살표 함수 괄호 사용 방식
+  singleQuote: true, // 큰따옴표를 작은따옴표로 자동수정
+  semi: true, // 세미콜론 자동수정
+};
