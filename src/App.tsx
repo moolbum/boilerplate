77634f499007129c48b239d3e1f188105f2a12d6
slogup-dale <dyddn304@gmail.com>
2024-01-09 11:27:55 +0900

    build: Styled component 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import styled from "styled-components";
 
 function App() {
-  return <div>Hello world</div>;
+  return <Head>Hello world</Head>;
 }
 
 export default App;
+
+const Head = styled.h1`
+  font-size: 20px;
+`;
