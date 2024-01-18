eb6ab546d0a25af56a63293b86f7557d3290da67
slogup-dale <dyddn304@gmail.com>
2024-01-18 10:54:02 +0900

    feat: toggle 커스텀 훅 추가

--- src/components/atom/Dialog/index.tsx
+++ src/components/atom/Dialog/index.tsx
@@ -1,83 +1,61 @@
-import React, {
-  ButtonHTMLAttributes,
-  HTMLAttributes,
-  PropsWithChildren,
-  ReactNode,
-  createContext,
-  useContext,
-} from 'react';
+import React, { HTMLAttributes, PropsWithChildren, ReactNode, createContext, useContext } from 'react';
 import Typo from '../Typo';
 import { createPortal } from 'react-dom';
 import { styled } from 'styled-components';
+import { colors } from '@/styles/color';
+import useToggleProvider from '@/hooks/useToggleProvider';
 
 interface DialogContextProps {
   isOpen: boolean;
-  onOpenChange?: () => void;
+  onOpenChange: (open: boolean) => void;
 }
-export const DialogContext = createContext<DialogContextProps | undefined>(undefined);
 
-// Dialog Main 최상단 컴포넌트
+/** Dialog Root 최상단 컴포넌트*/
 interface DialogMainProps {
-  open: boolean;
-  onOpenChange?: () => void;
+  open?: boolean;
+  onOpenChange?: React.Dispatch<React.SetStateAction<boolean>>;
 }
-function DialogMain({ children, open, onOpenChange = undefined }: PropsWithChildren<DialogMainProps>) {
-  const values = {
-    isOpen: open,
+const DialogContext = createContext<DialogContextProps | undefined>(undefined);
+function DialogRoot({ children, open = undefined, onOpenChange = undefined }: PropsWithChildren<DialogMainProps>) {
+  const { values } = useToggleProvider({
+    open: open,
     onOpenChange,
-  };
+  });
 
   return <DialogContext.Provider value={values}>{children}</DialogContext.Provider>;
 }
 
-// Dialog Header
-function DialogHeader({ children }: PropsWithChildren) {
-  return <>{children}</>;
+/** Dialog Toggle */
+function DialogCloseToggle({ children }: PropsWithChildren) {
+  const { onOpenChange } = useContext(DialogContext as React.Context<DialogContextProps>);
+
+  return <button onClick={() => onOpenChange(false)}>{children}</button>;
 }
 
-// Dialog Content
-function DialogContent({ children }: PropsWithChildren) {
-  return <>{children}</>;
-}
-
-// Dialog Footer
-function DialogFooter({ children }: PropsWithChildren) {
-  return <>{children}</>;
-}
-
-// Dialog Overlay
-function DialogOverlay(props: HTMLAttributes<HTMLDivElement>) {
-  return <div {...props} />;
-}
-
-// Dialog Portal
-export function DialogPortal({ children }: PropsWithChildren) {
+/** Dialog Portal */
+function DialogPortal({ children }: PropsWithChildren) {
   const { isOpen } = useContext(DialogContext as React.Context<DialogContextProps>);
 
   return isOpen ? createPortal(<>{children}</>, document.body) : null;
 }
 
-// Dialog ConfirmButton
-interface DialogConfirmButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
-function DialogConfirmButton(props: DialogConfirmButtonProps) {
-  return <button {...props} />;
+/** Dialog Overlay */
+function DialogOverlay(props: HTMLAttributes<HTMLDivElement>) {
+  const { onOpenChange } = useContext(DialogContext as React.Context<DialogContextProps>);
+
+  return <div {...props} onClick={() => onOpenChange(false)} />;
 }
 
-// Dialog CloseButton
-interface DialogCloseButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
-function DialogCloseButton(props: DialogCloseButtonProps) {
-  return <button {...props} />;
+/** Dialog Content */
+function DialogContent(props: PropsWithChildren<HTMLAttributes<HTMLDivElement>>) {
+  return <div {...props} />;
 }
 
-// Dialog Quantums
-const QDialog = Object.assign(DialogMain, {
-  Header: DialogHeader,
+const QDialog = Object.assign(DialogRoot, {
   Content: DialogContent,
-  Footer: DialogFooter,
   Portal: DialogPortal,
   Overlay: DialogOverlay,
-  OkButton: DialogConfirmButton,
-  CloseButton: DialogCloseButton,
+  CloseButton: DialogCloseToggle,
 });
 
 // Dialog
@@ -85,6 +63,7 @@ interface DialogProps {
   open?: boolean;
   onOpenChange?: () => void;
   title?: string;
+  isCloseButton?: boolean;
   rightAccessary?: ReactNode;
   footer?: ReactNode;
 }
@@ -95,19 +74,22 @@ function Dialog({
   rightAccessary,
   children,
   footer,
+  isCloseButton,
 }: PropsWithChildren<DialogProps>) {
   return (
     <QDialog open={open} onOpenChange={onOpenChange}>
-      <QDialog.Portal />
-      <StyledOverlay />
+      <QDialog.Portal>
+        <StyledOverlay />
 
-      <StyledHeader>
-        <Typo>{title}</Typo>
-        {rightAccessary}
-      </StyledHeader>
+        <StyledHeader>
+          <Typo>{title}</Typo>
+          {rightAccessary}
+          {isCloseButton && <QDialog.CloseButton />}
+        </StyledHeader>
 
-      <QDialog.Content>{children}</QDialog.Content>
-      <QDialog.Footer>{footer}</QDialog.Footer>
+        <StyledContent>{children}</StyledContent>
+        <StyledFooter>{footer}</StyledFooter>
+      </QDialog.Portal>
     </QDialog>
   );
 }
@@ -117,13 +99,19 @@ export default Dialog;
 const StyledOverlay = styled(QDialog.Overlay)`
   position: fixed;
   inset: 0;
-  background: rgba(0, 0, 0, 0.5);
+  background: ${colors.greyOpacity600};
   width: 100%;
   height: 100vh;
 `;
 
-const StyledHeader = styled(QDialog.Header)`
+const StyledHeader = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
 `;
+
+const StyledContent = styled(QDialog.Content)`
+  background: ${colors.white};
+`;
+
+const StyledFooter = styled.div``;
--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LocalIcon from '@/components/atom/Icons/LocalIcon';
 import LucideIcon from '@/components/atom/Icons/LucideIcon';
 import RadixIcon from '@/components/atom/Icons/RadixIcon';
@@ -11,248 +11,272 @@ import Grid from '@/components/molecule/Grid';
 import Button from '@/components/atom/Button';
 import TextArea from '@/components/atom/TextArea';
 import Callout from '@/components/molecule/Callout';
+import Dialog from '@/components/atom/Dialog';
+
+type DialogState = null | 'OPEN_TEST';
 
 function MainPage() {
+  const [isDialogOpen, setIsDialogOpen] = useState<DialogState>(null);
+
+  const renderDialog = () => {
+    if (isDialogOpen === 'OPEN_TEST') {
+      return (
+        <Dialog open onOpenChange={() => setIsDialogOpen(null)}>
+          <div>오픈</div>
+        </Dialog>
+      );
+    }
+  };
+
   return (
-    <Flex direction="column">
-      <Typo as="h1" role="title" fontWeight="semibold" color="red500">
-        h1 MainPage
-      </Typo>
-      <Typo as="h2" fontWeight="bold">
-        h2 MainPage
-      </Typo>
-      <Typo as="p">P MainPage</Typo>
-      <Typo>Default MainPage</Typo>
-      <Typo isUnderLine underLineThickness={4} underlineOffset={2}>
-        Under Line 20
-      </Typo>
-      <Typo>Under Line</Typo>
-
-      <Flex gap={8}>
-        <Typo as="h1" typo="h1" fontWeight="bold">
-          Icon
+    <>
+      <Flex direction="column">
+        <Typo as="h1" role="title" fontWeight="semibold" color="red500">
+          h1 MainPage
         </Typo>
-        <RadixIcon name="ActivityLogIcon" size={16} />
-        <LucideIcon name="ALargeSmall" size={16} />
-        <LocalIcon name={TableSvg} size={16} />
+        <Typo as="h2" fontWeight="bold">
+          h2 MainPage
+        </Typo>
+        <Typo as="p">P MainPage</Typo>
+        <Typo>Default MainPage</Typo>
+        <Typo isUnderLine underLineThickness={4} underlineOffset={2}>
+          Under Line 20
+        </Typo>
+        <Typo>Under Line</Typo>
+
+        <Flex gap={8}>
+          <Typo as="h1" typo="h1" fontWeight="bold">
+            Icon
+          </Typo>
+          <RadixIcon name="ActivityLogIcon" size={16} />
+          <LucideIcon name="ALargeSmall" size={16} />
+          <LocalIcon name={TableSvg} size={16} />
+        </Flex>
+
+        <Flex>
+          <Badge color="gray">test</Badge>
+          <Badge variant="outline">outline</Badge>
+          <Badge color="lime" variant="outline">
+            outline
+          </Badge>
+          <Badge color="lime" variant="solid">
+            데일
+          </Badge>
+        </Flex>
+
+        <SectionCard>SectionCard</SectionCard>
+
+        <Grid>
+          <Grid.Col columnSpan={4}>
+            <Badge color="lime" variant="solid">
+              데일
+            </Badge>
+          </Grid.Col>
+          <Grid.Col>
+            <Badge color="lime" variant="solid">
+              데일
+            </Badge>
+          </Grid.Col>
+          <Grid.Col>
+            <Badge color="lime" variant="solid">
+              데일
+            </Badge>
+          </Grid.Col>
+          <Grid.Col>
+            <Badge color="lime" variant="solid">
+              데일
+            </Badge>
+          </Grid.Col>
+          <Grid.Col>
+            <Badge color="lime" variant="solid">
+              데일
+            </Badge>
+          </Grid.Col>
+          <Grid.Col>
+            <Badge color="lime" variant="solid">
+              데일
+            </Badge>
+          </Grid.Col>
+          <Grid.Col>
+            <Badge color="lime" variant="solid">
+              데일
+            </Badge>
+          </Grid.Col>
+          <Grid.Col>
+            <Badge color="lime" variant="solid">
+              데일
+            </Badge>
+          </Grid.Col>
+          <Grid.Col>
+            <Badge color="lime" variant="solid">
+              데일
+            </Badge>
+          </Grid.Col>
+        </Grid>
+
+        {/* soft Button */}
+        <Flex>
+          <Button size="large" color="blue">
+            button
+          </Button>
+          <Button size="large" color="gray">
+            button
+          </Button>
+          <Button size="large" color="green">
+            button
+          </Button>
+          <Button size="large" color="lime">
+            button
+          </Button>
+          <Button size="large" color="mint">
+            button
+          </Button>
+          <Button size="large" color="orange">
+            button
+          </Button>
+          <Button size="large" color="pink">
+            button
+          </Button>
+          <Button size="large" color="purple">
+            button
+          </Button>
+          <Button size="large" color="red">
+            button
+          </Button>
+          <Button size="large" color="sky">
+            button
+          </Button>
+          <Button size="large" color="yellow">
+            button
+          </Button>
+        </Flex>
+
+        {/* solid Button */}
+        <Flex>
+          <Button variant="solid" size="large" color="blue">
+            button
+          </Button>
+          <Button variant="solid" size="large" color="gray">
+            button
+          </Button>
+          <Button variant="solid" size="large" color="green">
+            button
+          </Button>
+          <Button variant="solid" size="large" color="lime">
+            button
+          </Button>
+          <Button variant="solid" size="large" color="mint">
+            button
+          </Button>
+          <Button variant="solid" size="large" color="orange">
+            button
+          </Button>
+          <Button variant="solid" size="large" color="pink">
+            button
+          </Button>
+          <Button variant="solid" size="large" color="purple">
+            button
+          </Button>
+          <Button variant="solid" size="large" color="red">
+            button
+          </Button>
+          <Button variant="solid" size="large" color="sky">
+            button
+          </Button>
+          <Button variant="solid" size="large" color="yellow">
+            button
+          </Button>
+        </Flex>
+
+        {/* outline Button */}
+        <Flex>
+          <Button variant="outline" size="large" color="blue">
+            button
+          </Button>
+          <Button variant="outline" size="large" color="gray">
+            button
+          </Button>
+          <Button variant="outline" size="large" color="green">
+            button
+          </Button>
+          <Button variant="outline" size="large" color="lime">
+            button
+          </Button>
+          <Button variant="outline" size="large" color="mint">
+            button
+          </Button>
+          <Button variant="outline" size="large" color="orange">
+            button
+          </Button>
+          <Button variant="outline" size="large" color="pink">
+            button
+          </Button>
+          <Button variant="outline" size="large" color="purple">
+            button
+          </Button>
+          <Button variant="outline" size="large" color="red">
+            button
+          </Button>
+          <Button variant="outline" size="large" color="sky">
+            button
+          </Button>
+          <Button variant="outline" size="large" color="yellow">
+            button
+          </Button>
+        </Flex>
+
+        {/* ghost Button */}
+        <Flex>
+          <Button variant="ghost" size="large" color="blue">
+            button
+          </Button>
+          <Button variant="ghost" size="large" color="gray">
+            button
+          </Button>
+          <Button variant="ghost" size="large" color="green">
+            button
+          </Button>
+          <Button variant="ghost" size="large" color="lime">
+            button
+          </Button>
+          <Button variant="ghost" size="large" color="mint">
+            button
+          </Button>
+          <Button variant="ghost" size="large" color="orange">
+            button
+          </Button>
+          <Button variant="ghost" size="large" color="pink">
+            button
+          </Button>
+          <Button variant="ghost" size="large" color="purple">
+            button
+          </Button>
+          <Button variant="ghost" size="large" color="red">
+            button
+          </Button>
+          <Button variant="ghost" size="large" color="sky">
+            button
+          </Button>
+          <Button variant="ghost" size="large" color="yellow">
+            button
+          </Button>
+        </Flex>
+
+        <TextArea />
+        <div>
+          <Callout icon={<Typo>Icon</Typo>}>
+            <Typo>test</Typo>
+          </Callout>
+        </div>
+
+        <Button onClick={() => setIsDialogOpen('OPEN_TEST')}>Dialog Open</Button>
       </Flex>
+      {renderDialog()}
 
-      <Flex>
-        <Badge color="gray">test</Badge>
-        <Badge variant="outline">outline</Badge>
-        <Badge color="lime" variant="outline">
-          outline
-        </Badge>
-        <Badge color="lime" variant="solid">
-          데일
-        </Badge>
-      </Flex>
-
-      <SectionCard>SectionCard</SectionCard>
-
-      <Grid>
-        <Grid.Col columnSpan={4}>
-          <Badge color="lime" variant="solid">
-            데일
-          </Badge>
-        </Grid.Col>
-        <Grid.Col>
-          <Badge color="lime" variant="solid">
-            데일
-          </Badge>
-        </Grid.Col>
-        <Grid.Col>
-          <Badge color="lime" variant="solid">
-            데일
-          </Badge>
-        </Grid.Col>
-        <Grid.Col>
-          <Badge color="lime" variant="solid">
-            데일
-          </Badge>
-        </Grid.Col>
-        <Grid.Col>
-          <Badge color="lime" variant="solid">
-            데일
-          </Badge>
-        </Grid.Col>
-        <Grid.Col>
-          <Badge color="lime" variant="solid">
-            데일
-          </Badge>
-        </Grid.Col>
-        <Grid.Col>
-          <Badge color="lime" variant="solid">
-            데일
-          </Badge>
-        </Grid.Col>
-        <Grid.Col>
-          <Badge color="lime" variant="solid">
-            데일
-          </Badge>
-        </Grid.Col>
-        <Grid.Col>
-          <Badge color="lime" variant="solid">
-            데일
-          </Badge>
-        </Grid.Col>
-      </Grid>
-
-      {/* soft Button */}
-      <Flex>
-        <Button size="large" color="blue">
-          button
-        </Button>
-        <Button size="large" color="gray">
-          button
-        </Button>
-        <Button size="large" color="green">
-          button
-        </Button>
-        <Button size="large" color="lime">
-          button
-        </Button>
-        <Button size="large" color="mint">
-          button
-        </Button>
-        <Button size="large" color="orange">
-          button
-        </Button>
-        <Button size="large" color="pink">
-          button
-        </Button>
-        <Button size="large" color="purple">
-          button
-        </Button>
-        <Button size="large" color="red">
-          button
-        </Button>
-        <Button size="large" color="sky">
-          button
-        </Button>
-        <Button size="large" color="yellow">
-          button
-        </Button>
-      </Flex>
-
-      {/* solid Button */}
-      <Flex>
-        <Button variant="solid" size="large" color="blue">
-          button
-        </Button>
-        <Button variant="solid" size="large" color="gray">
-          button
-        </Button>
-        <Button variant="solid" size="large" color="green">
-          button
-        </Button>
-        <Button variant="solid" size="large" color="lime">
-          button
-        </Button>
-        <Button variant="solid" size="large" color="mint">
-          button
-        </Button>
-        <Button variant="solid" size="large" color="orange">
-          button
-        </Button>
-        <Button variant="solid" size="large" color="pink">
-          button
-        </Button>
-        <Button variant="solid" size="large" color="purple">
-          button
-        </Button>
-        <Button variant="solid" size="large" color="red">
-          button
-        </Button>
-        <Button variant="solid" size="large" color="sky">
-          button
-        </Button>
-        <Button variant="solid" size="large" color="yellow">
-          button
-        </Button>
-      </Flex>
-
-      {/* outline Button */}
-      <Flex>
-        <Button variant="outline" size="large" color="blue">
-          button
-        </Button>
-        <Button variant="outline" size="large" color="gray">
-          button
-        </Button>
-        <Button variant="outline" size="large" color="green">
-          button
-        </Button>
-        <Button variant="outline" size="large" color="lime">
-          button
-        </Button>
-        <Button variant="outline" size="large" color="mint">
-          button
-        </Button>
-        <Button variant="outline" size="large" color="orange">
-          button
-        </Button>
-        <Button variant="outline" size="large" color="pink">
-          button
-        </Button>
-        <Button variant="outline" size="large" color="purple">
-          button
-        </Button>
-        <Button variant="outline" size="large" color="red">
-          button
-        </Button>
-        <Button variant="outline" size="large" color="sky">
-          button
-        </Button>
-        <Button variant="outline" size="large" color="yellow">
-          button
-        </Button>
-      </Flex>
-
-      {/* ghost Button */}
-      <Flex>
-        <Button variant="ghost" size="large" color="blue">
-          button
-        </Button>
-        <Button variant="ghost" size="large" color="gray">
-          button
-        </Button>
-        <Button variant="ghost" size="large" color="green">
-          button
-        </Button>
-        <Button variant="ghost" size="large" color="lime">
-          button
-        </Button>
-        <Button variant="ghost" size="large" color="mint">
-          button
-        </Button>
-        <Button variant="ghost" size="large" color="orange">
-          button
-        </Button>
-        <Button variant="ghost" size="large" color="pink">
-          button
-        </Button>
-        <Button variant="ghost" size="large" color="purple">
-          button
-        </Button>
-        <Button variant="ghost" size="large" color="red">
-          button
-        </Button>
-        <Button variant="ghost" size="large" color="sky">
-          button
-        </Button>
-        <Button variant="ghost" size="large" color="yellow">
-          button
-        </Button>
-      </Flex>
-
-      <TextArea />
-      <div>
-        <Callout icon={<Typo>Icon</Typo>}>
-          <Typo>test</Typo>
-        </Callout>
-      </div>
-    </Flex>
+      <Dialog>
+        <div>그냥 열림? </div>
+      </Dialog>
+    </>
   );
 }
 
