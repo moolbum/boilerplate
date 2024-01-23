65b1ed6a281ce64f06cb7d7445589e47e942941b
slogup-dale <dyddn304@gmail.com>
2024-01-23 11:11:01 +0900

    chore: zIndex 상수수정

--- src/styles/zIndex.ts
+++ src/styles/zIndex.ts
@@ -1,6 +1,9 @@
 export const zIndex = {
-  dialogContent: 1000,
   dialogOverlay: 900,
+  dialogContent: 1000,
+  alertDialogOverlay: 1100,
+  alertDialogContent: 1200,
+  toast: 9999,
 };
 
 export type ZIndexType = keyof typeof zIndex;
