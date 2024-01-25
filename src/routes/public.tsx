c6502aae1ae19a64858287ef9143731d02952033
slogup-dale <dyddn304@gmail.com>
2024-01-26 08:53:02 +0900

    Revert "refactor: 폴더구조 정리"
    This reverts commit 5b0feff655a8f155af101eef69a6f31cef98e791.

--- src/routes/public.tsx
+++ src/routes/public.tsx
@@ -2,7 +2,7 @@ import { RouteObject } from 'react-router-dom';
 import PATH from './path';
 import ProtectedPublicRoute from '../components/organisms/ProtectedPublicRoute';
 import { MainPage } from '@src/pages';
-import { MainPageLayout } from '@/components/organisms';
+import { MainPageLayout } from '@/components/templates';
 
 const publicRoute: RouteObject[] = [
   {
