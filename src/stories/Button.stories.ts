2342b7de549d760dac6f3081a0dd41a07585503d
slogup-dale <dyddn304@gmail.com>
2024-01-26 11:33:41 +0900

    update: 버튼 스토리북 수정

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Button } from './Button';
 
 const meta = {
-  title: 'Example/Button',
+  title: 'Button',
   component: Button,
   parameters: {
     layout: 'centered',
@@ -16,12 +16,12 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   args: {
     primary: true,
-    label: 'Button',
+    children: 'button',
   },
 };
 
 export const Secondary: Story = {
   args: {
-    label: 'Button',
+    children: 'button',
   },
 };
--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
+import React, { ButtonHTMLAttributes } from 'react';
 import './button.css';
 
-interface ButtonProps {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   primary?: boolean;
   backgroundColor?: string;
   size?: 'small' | 'medium' | 'large';
-  label: string;
   onClick?: () => void;
 }
 
-export const Button = ({ primary = false, size = 'medium', backgroundColor, label, ...props }: ButtonProps) => {
+export const Button = ({ primary = false, size = 'medium', backgroundColor, ...props }: ButtonProps) => {
   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
 
   return (
@@ -18,8 +17,6 @@ export const Button = ({ primary = false, size = 'medium', backgroundColor, labe
       className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
       style={{ backgroundColor }}
       {...props}
-    >
-      {label}
-    </button>
+    />
   );
 };
