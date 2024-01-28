ee8603662dd08dd0631afb7be7dc784d81cf50b6
slogup-dale <dyddn304@gmail.com>
2024-01-28 18:07:50 +0900

    feat: 스토리북 스위치 추가

--- src/stories/Swtich.stories.ts
+++ src/stories/Swtich.stories.ts
@@ -1,18 +1,37 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import Swtich from './Swtich';
+import { colors } from '@/styles/color';
 
 const meta = {
-  title: 'Button',
+  title: 'Swtich',
   component: Swtich,
   parameters: {
     layout: 'centered',
   },
   tags: ['autodocs'],
+  argTypes: {
+    size: {
+      options: ['small', 'medium', 'large'],
+      control: { type: 'radio' },
+    },
+    color: {
+      options: Object.entries(colors).map(color => color[0]),
+      control: { type: 'select' },
+    },
+    disabledColor: {
+      options: Object.entries(colors).map(color => color[0]),
+      control: { type: 'select' },
+    },
+  },
 } satisfies Meta<typeof Swtich>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
-  args: { checked: true },
+  args: {
+    checked: true,
+    disabled: false,
+    size: 'small',
+  },
 };
