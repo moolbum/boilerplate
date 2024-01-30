ca020aa7e0f43e92d1befbffd913f02907faaf17
slogup-dale <dyddn304@gmail.com>
2024-01-30 10:32:07 +0900

    refactor: types 파일정리

---
+++ src/types/index.ts
@@ -0,0 +1,2 @@
+export type { CommonVariantType, BadgeVariantType, ButtonVariantType } from './variant';
+export type { CommonSizeType, BadgeSizeType, CalloutSizeType, ButtonSizeType } from './size';
